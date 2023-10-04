7dc2ce12bd1d18b3eb31b64e26c0d9752a774120
Kyle Labelle <klabe099@uottawa.ca>
2023-10-04 11:16:40 -0400

    added Iterator tests for all types

--- code_part2/Part2.java
+++ code_part2/Part2.java
@@ -9,19 +9,37 @@ public class Part2 {
         int size = 1000000;
         int[] randomIntegers = generateRandomIntegers(size);
 
-        long arrayListTime = 0;
-        long vectorTime = 0;
-        long arrayTime = 0;
+        
+        //Construction Tests
+        long arrayListConstructionTime = 0;
+        long vectorConstructionTime = 0;
+        long arrayConstructionTime = 0;
 
-        for (int i = 0; i < 2250; i++) { //about 10 seconds
-            arrayListTime += testArrayListConstruction(randomIntegers);
-            vectorTime += testVectorConstruction(randomIntegers);
-            arrayTime += testArrayConstruction(randomIntegers);
+        for (int i = 0; i < 2250; i++) { //about 10 seconds for ArrayList
+            arrayListConstructionTime += testArrayListConstruction(randomIntegers);
+            vectorConstructionTime += testVectorConstruction(randomIntegers);
+            arrayConstructionTime += testArrayConstruction(randomIntegers);
         }
 
-        System.out.println("ArrayList construction time: " + arrayListTime + " ms");
-        System.out.println("Vector construction time: " + vectorTime + " ms");
-        System.out.println("Array construction time: " + arrayTime + " ms");
+        System.out.println("ArrayList construction time: " + arrayListConstructionTime + " ms");
+        System.out.println("Vector construction time: " + vectorConstructionTime + " ms");
+        System.out.println("Array construction time: " + arrayConstructionTime + " ms");
+        
+
+        //Iteration Tests
+        long arrayListIterationTime = 0;
+        long vectorIterationTime = 0;
+        long arrayIterationTime = 0;
+
+        for (int i = 0; i < 10000; i++) { //about 10 seconds for ArrayList
+            arrayListIterationTime += testArrayListIteration(randomIntegers);
+            vectorIterationTime += testVectorIteration(randomIntegers);
+            arrayIterationTime += testArrayIteration(randomIntegers);
+        }
+
+        System.out.println("ArrayList iteration time: " + arrayListIterationTime + " ms");
+        System.out.println("Vector iteration time: " + vectorIterationTime + " ms");
+        System.out.println("Array iteration time: " + arrayIterationTime + " ms");
     }
 
     private static int[] generateRandomIntegers(int size) {
@@ -58,6 +76,50 @@ public class Part2 {
         int[] array = new int[randomIntegers.length];
         for(int i = 0; i < randomIntegers.length; i++) {
             array[i] = randomIntegers[i];
+        }
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    private static long testArrayListIteration(int[] randomIntegers) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for(int num : randomIntegers) {
+            arrayList.add(num);
+        }
+        int sum = 0;
+        long startTime = System.currentTimeMillis();
+        Iterator<Integer> iterator = arrayList.iterator();
+        while(iterator.hasNext()) {
+            sum += iterator.next();
+        }
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    private static long testVectorIteration(int[] randomIntegers) {
+        Vector<Integer> vector = new Vector<>();
+        for(int num : randomIntegers) {
+            vector.add(num);
+        }
+        int sum = 0;
+        long startTime = System.currentTimeMillis();
+        Iterator<Integer> iterator = vector.iterator();
+        while(iterator.hasNext()) {
+            sum += iterator.next();
+        }
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
+
+    private static long testArrayIteration(int[] randomIntegers) {
+        int[] array = new int[randomIntegers.length];
+        for(int i = 0; i < randomIntegers.length; i++) {
+            array[i] = randomIntegers[i];
+        }
+        int sum = 0;
+        long startTime = System.currentTimeMillis();
+        for(int num : array) {
+            sum += num;
         }
         long endTime = System.currentTimeMillis();
         return endTime - startTime;
