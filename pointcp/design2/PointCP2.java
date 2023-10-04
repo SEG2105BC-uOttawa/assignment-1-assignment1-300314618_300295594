bbecffed41b6f5cec341f03c31337b02689e268e
Bthan <gamzpro52@gmail.com>
2023-10-04 16:03:52 -0400

    added design 2

---
+++ pointcp/design2/PointCP2.java
@@ -0,0 +1,139 @@
+// This file contains material supporting section 2.9 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at http://www.site.uottawa.ca/school/research/lloseng/
+
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+public class PointCP2
+{
+  //Instance variables ************************************************
+
+  /**
+   * Contains C(artesian) or P(olar) to identify the type of
+   * coordinates that are being dealt with.
+   */
+  private char typeCoord;
+  
+  /**
+   * Contains the current value of X or RHO depending on the type
+   * of coordinates.
+   */
+  private double xOrRho;
+  
+  /**
+   * Contains the current value of Y or THETA value depending on the
+   * type of coordinates.
+   */
+  private double yOrTheta;
+
+  private double rho;
+  private double theta;
+	
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public PointCP2(double rho, double theta)
+  {
+    this.rho = rho;
+    this.theta = theta;
+  }
+	
+  
+  //Instance methods **************************************************
+ 
+  //rounding down to 0 if less than 1E-10
+  public double getX()
+  {
+    double temp = Math.cos(Math.toRadians(theta)) * rho;
+    return temp < 1.0E-10 ? 0 : temp;
+  }
+  
+  public double getY()
+  {
+    double temp = Math.sin(Math.toRadians(theta)) * rho;
+    return temp < 1.0E-10 ? 0 : temp;
+  }
+  
+  public double getRho()
+  {
+    return rho;
+  }
+  
+  public double getTheta()
+  {
+    return theta;
+  }
+  
+	
+  /**
+   * Converts Cartesian coordinates to Polar coordinates.
+   */
+  public void convertStorageToPolar()
+  {
+    //Empty for this design
+  }
+	
+  /**
+   * Converts Polar coordinates to Cartesian coordinates.
+   */
+  public void convertStorageToCartesian()
+  {
+    //Empty for this design
+  }
+
+  /**
+   * Calculates the distance in between two points using the Pythagorean
+   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+   *
+   * @param pointA The first point.
+   * @param pointB The second point.
+   * @return The distance between the two points.
+   */
+  public double getDistance(PointCP2 pointB)
+  {
+    // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double deltaX = getX() - pointB.getX();
+    double deltaY = getY() - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  }
+
+  /**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+  public PointCP2 rotatePoint(double rotation)
+  {
+    double radRotation = Math.toRadians(rotation);
+    double newX = (Math.cos(radRotation) * getX()) - (Math.sin(radRotation) * getY());
+    double newY = (Math.sin(radRotation) * getY()) + (Math.cos(radRotation) * getY());
+
+    return new PointCP2(newX, newY);
+  }
+
+  /**
+   * Returns information about the coordinates.
+   *
+   * @return A String containing information about the coordinates.
+   */
+  public String toString()
+  {
+    return "Polar (" + getRho() + "," + getTheta() + ")";
+  }
+}
